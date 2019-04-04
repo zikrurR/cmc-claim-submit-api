5c79a98a97d64233e50e9526dfbd64ccaf51e010
alexsoliri <alexis.gayte@solirius.com>
2019-04-04 23:13:43 +0100

    add parameter

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,15 +1,3 @@
 output "microserviceName" {
   value = "${var.component}"
 }
-
-output "s2s_url" {
-  value = "${local.s2s_url}"
-}
-
-output "s2s_secret" {
-  value = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-}
-
-output "ccd_url" {
-  value = "${local.ccd_url}"
-}
