9fcd5dcc7ffd81693fd0b18de442199ce48dbd25
alexsoliri <alexis.gayte@solirius.com>
2019-04-04 13:52:41 +0100

    give a try on adding params

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,10 @@
 provider "azurerm" {}
 
 locals {
-  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  local_env   = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  ase_name    = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  s2s_url     = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  ccd_url     = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
 }
 
 module "cmc-claim-submit-api" {
@@ -10,7 +13,6 @@ module "cmc-claim-submit-api" {
   location            = "${var.location_app}"
   env                 = "${var.env}"
   ilbIp               = "${var.ilbIp}"
-#  resource_group_name = "${azurerm_resource_group.rg.name}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
@@ -19,4 +21,9 @@ module "cmc-claim-submit-api" {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
   }
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name = "claim-store-s2s-secret"
+  vault_uri = "${data.azurerm_key_vault.cmc_key_vault.vault_uri}"
 }
