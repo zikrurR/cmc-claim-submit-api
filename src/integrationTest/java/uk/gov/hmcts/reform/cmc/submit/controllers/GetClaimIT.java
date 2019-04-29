4089a8ff3ec012c8a13d79b1d0ae48374c16597f
alexsoliri <alexis.gayte@solirius.com>
2019-04-29 16:04:36 +0100

    add integration on data conversion.

--- src/integrationTest/java/uk/gov/hmcts/reform/cmc/submit/controllers/GetClaimIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cmc/submit/controllers/GetClaimIT.java
@@ -18,13 +18,41 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.AmountType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdAmountRow;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdApplicant;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdEvidenceRow;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdEvidenceType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdInterestDateType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdInterestEndDateType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdInterestType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdPartyType;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdRespondent;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.CcdTimelineEvent;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdAddressBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdAmountRowBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdApplicantBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdCaseBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdCollectionElementBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdEvidenceRowBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdPartyBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdRespondentBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdTelephoneBuilder;
+import uk.gov.hmcts.reform.cmc.submit.ccd.domain.builders.CcdTimelineEventBuilder;
 import uk.gov.hmcts.reform.cmc.submit.domain.models.Claim;
 import uk.gov.hmcts.reform.cmc.submit.domain.models.ClaimInput;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.amount.AmountBreakDown;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.amount.AmountRow;
 import uk.gov.hmcts.reform.cmc.submit.domain.models.amount.NotKnown;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.claimants.Individual;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.defendants.IndividualDetails;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.evidence.Evidence;
 import uk.gov.hmcts.reform.cmc.submit.domain.models.interest.Interest;
 import uk.gov.hmcts.reform.cmc.submit.domain.models.payment.ReferencePayment;
+import uk.gov.hmcts.reform.cmc.submit.domain.models.timeline.TimelineEvent;
 import uk.gov.hmcts.reform.cmc.submit.domain.samples.SampleClaimData;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
@@ -34,7 +62,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.reform.cmc.ccd.assertion.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -90,131 +117,121 @@ public class GetClaimIT {
     @Test
     public void mappingDataTest() throws Exception {
 
-        //
-
         ClaimInput validDefaults = SampleClaimData.validDefaults();
+
+        // mock ccd call
+        CcdCaseBuilder builder = CcdCaseBuilder.builder();
+
+        Individual individual = (Individual)validDefaults.getClaimants().get(0);
+        builder.applicants(Arrays.asList(CcdCollectionElementBuilder.<CcdApplicant>builder()
+                .id(individual.getId())
+                .value(CcdApplicantBuilder.builder()
+                        .partyName("John Rambo")
+                        .representativeOrganisationName("Trading ltd")
+                        .representativeOrganisationEmail("representative@example.org")
+                        .representativeOrganisationDxAddress("DX123456")
+                        .representativeOrganisationPhone("7873738547")
+                        .representativeOrganisationAddress(CcdAddressBuilder.builder()
+                                .addressLine1("52")
+                                .addressLine2("Down Street")
+                                .addressLine3("Salford")
+                                .postTown("Manchester")
+                                .postCode("DF1 3LJ"))
+                        .partyDetail(CcdPartyBuilder.builder()
+                                .dateOfBirth(LocalDate.parse("1968-01-02"))
+                                .type(CcdPartyType.INDIVIDUAL)
+                                .telephoneNumber(CcdTelephoneBuilder.builder()
+                                        .telephoneNumber("07873727165"))
+                                .correspondenceAddress(CcdAddressBuilder.builder()
+                                        .addressLine1("52")
+                                        .addressLine2("Down Street")
+                                        .addressLine3("Salford")
+                                        .postTown("Manchester")
+                                        .postCode("DF1 3LJ"))
+                                .primaryAddress(CcdAddressBuilder.builder()
+                                        .addressLine1("52")
+                                        .addressLine2("Down Street")
+                                        .addressLine3("Salford")
+                                        .postTown("Manchester")
+                                        .postCode("DF1 3LJ"))
+                                )
+                        )
+
+                ));
+
+        Evidence evidence = validDefaults.getEvidences().get(0);
+        builder.evidence(Arrays.asList(CcdCollectionElementBuilder.<CcdEvidenceRow>builder()
+                .id(evidence.getId())
+                .value(CcdEvidenceRowBuilder.builder()
+                        .type(CcdEvidenceType.CORRESPONDENCE)
+                        .description(evidence.getDescription()))
+                ));
+
+        AmountRow amountRow = ((AmountBreakDown)validDefaults.getAmount()).getRows().get(0);
+        builder.amountBreakDown(Arrays.asList(CcdCollectionElementBuilder.<CcdAmountRow>builder()
+                .id(amountRow.getId())
+                .value(CcdAmountRowBuilder.builder()
+                        .reason(amountRow.getReason())
+                        .amount(amountRow.getAmount()))
+                ));
+
+        TimelineEvent timeline = validDefaults.getTimeline().get(0);
+        builder.timeline(Arrays.asList(CcdCollectionElementBuilder.<CcdTimelineEvent>builder()
+                .id(timeline.getId())
+                .value(CcdTimelineEventBuilder.builder()
+                        .description(timeline.getDescription())
+                        .date(timeline.getDate()))
+                ));
+
+        IndividualDetails individualDetails = (IndividualDetails)validDefaults.getDefendants().get(0);
+        builder.respondents(Arrays.asList(CcdCollectionElementBuilder.<CcdRespondent>builder()
+                .id(individualDetails.getId())
+                .value(CcdRespondentBuilder.builder()
+                        .claimantProvidedDetail(CcdPartyBuilder.builder()
+                                .type(CcdPartyType.INDIVIDUAL)
+                                .emailAddress("j.smith@example.com")
+                                .primaryAddress(CcdAddressBuilder.builder()
+                                        .addressLine1("52")
+                                        .addressLine2("Down Street")
+                                        .addressLine3("Salford")
+                                        .postTown("Manchester")
+                                        .postCode("DF1 3LJ"))
+                                .lastName("Smith")
+                                .firstName("John")
+                                .title("Dr.")
+                                ))
+
+                ));
+
         ReferencePayment referencePayment = (ReferencePayment)validDefaults.getPayment();
         Interest interest = validDefaults.getInterest();
 
-        // mock amount
-        Map<String,Object> amountBreakDown = Maps.newHashMap();
-        amountBreakDown.put("id", "359fda9d-e5fd-4d6e-9525-238642d0157d");
-        Map<String,Object> amountBreakDownData = Maps.newHashMap();
-        amountBreakDownData.put("reason","reason");
-        amountBreakDownData.put("amount","40");
-        amountBreakDown.put("value", amountBreakDownData);
-
-        // mock evidence
-        Map<String,Object> evidences = Maps.newHashMap();
-        evidences.put("id", "d839f2f0-025f-4ee9-9a98-16bbe6ab3b35");
-        Map<String,Object> evidencesData = Maps.newHashMap();
-        evidencesData.put("type","CORRESPONDENCE");
-        evidencesData.put("description","description");
-        evidences.put("value", evidencesData);
-
-        // mock timeline
-        Map<String,Object> timelines = Maps.newHashMap();
-        timelines.put("id", "359fda9d-e5fd-4d6e-9525-238642d0157d");
-        Map<String,Object> timelinesData = Maps.newHashMap();
-        timelinesData.put("description", "signed a contract");
-        timelinesData.put("date", "Last Year");
-        timelines.put("value", timelinesData);
-
-        // mock applicants
-        Map<String,Object> applicants = Maps.newHashMap();
-        applicants.put("id", "acd82549-d279-4adc-b38c-d195dd0db0d6");
-        Map<String,Object> applicantsData = Maps.newHashMap();
-        applicantsData.put("partyName", "John Rambo");
-
-        Map<String,Object> partyDetailData = Maps.newHashMap();
-
-        Map<String,Object> primaryAddressData = Maps.newHashMap();
-        primaryAddressData.put("AddressLine1", "52");
-        primaryAddressData.put("AddressLine2", "Down Street");
-        primaryAddressData.put("AddressLine3", "Salford");
-        primaryAddressData.put("PostTown", "Manchester");
-        primaryAddressData.put("PostCode", "DF1 3LJ");
-
-        Map<String,Object> telephoneNumberData = Maps.newHashMap();
-        telephoneNumberData.put("telephoneNumber", "07873727165");
-
-        Map<String,Object> correspondenceAddressData = Maps.newHashMap();
-        correspondenceAddressData.put("AddressLine1", "52");
-        correspondenceAddressData.put("AddressLine2", "Down Street");
-        correspondenceAddressData.put("AddressLine3", "Salford");
-        correspondenceAddressData.put("PostTown", "Manchester");
-        correspondenceAddressData.put("PostCode", "DF1 3LJ");
-
-        partyDetailData.put("primaryAddress", primaryAddressData);
-        partyDetailData.put("telephoneNumber", telephoneNumberData);
-        partyDetailData.put("correspondenceAddress", correspondenceAddressData);
-        partyDetailData.put("dateOfBirth", "1968-01-02");
-        partyDetailData.put("type", "INDIVIDUAL");
-
-        applicantsData.put("partyDetail", partyDetailData);
-        Map<String,Object> contactDetailData = Maps.newHashMap();
-        contactDetailData.put("phone", "7873738547");
-        contactDetailData.put("email", "representative@example.com");
-        contactDetailData.put("dxAddress", "DX123456");
-
-        Map<String,Object> representativeOrganisationAddress = Maps.newHashMap();
-        representativeOrganisationAddress.put("AddressLine1", "52");
-        representativeOrganisationAddress.put("AddressLine2", "Down Street");
-        representativeOrganisationAddress.put("AddressLine3", "Salford");
-        representativeOrganisationAddress.put("PostTown", "Manchester");
-        representativeOrganisationAddress.put("PostCode", "DF1 3LJ");
-        applicantsData.put("representativeOrganisationAddress", representativeOrganisationAddress);
-        applicantsData.put("representativeOrganisationName", "Trading ltd");
-        applicantsData.put("representativeOrganisationEmail", "representative@example.org");
-        applicantsData.put("representativeOrganisationDxAddress", "DX123456");
-        applicantsData.put("representativeOrganisationPhone", "7873738547");
-
-        applicants.put("value", applicantsData);
-
-        // mock defendents
-        Map<String,Object> respondents = Maps.newHashMap();
-        respondents.put("id", "acd82549-d279-4adc-b38c-d195dd0db0d6");
-        Map<String,Object> respondentsData = Maps.newHashMap();
-        Map<String,Object> resPartyDetailData = Maps.newHashMap();
-
-        resPartyDetailData.put("type", "INDIVIDUAL");
-        respondentsData.put("claimantProvidedDetail", resPartyDetailData);
-        respondents.put("value", respondentsData);
-
-        // mock ccd call
-        Map<String,Object> mandatoryData = Maps.newHashMap();
-        mandatoryData.put("evidence", Arrays.asList(evidences));
-        mandatoryData.put("amountBreakDown", Arrays.asList(amountBreakDown));
-        mandatoryData.put("timeline", Arrays.asList(timelines));
-        mandatoryData.put("respondents", Arrays.asList(respondents));
-        mandatoryData.put("applicants", Arrays.asList(applicants));
-
-        mandatoryData.put("externalId", validDefaults.getExternalId().toString());
-        mandatoryData.put("referenceNumber", "random_reference_number");
-        mandatoryData.put("reason", validDefaults.getReason());
-        mandatoryData.put("amountType", "BREAK_DOWN");
-        mandatoryData.put("interestType", interest.getType().name());
-        mandatoryData.put("interestRate", interest.getRate());
-        mandatoryData.put("interestReason", interest.getReason());
-        mandatoryData.put("interestDateType", interest.getInterestDate().getType().name());
-        mandatoryData.put("interestClaimStartDate", interest.getInterestDate().getDate());
-        mandatoryData.put("interestStartDateReason", interest.getInterestDate().getReason());
-        mandatoryData.put("interestEndDateType", interest.getInterestDate().getEndDateType().name());
-        mandatoryData.put("paymentId", referencePayment.getId());
-        mandatoryData.put("paymentAmount", referencePayment.getAmount());
-        mandatoryData.put("paymentReference", referencePayment.getReference());
-        mandatoryData.put("paymentStatus", referencePayment.getStatus());
-        mandatoryData.put("paymentDateCreated", referencePayment.getDateCreated());
-        mandatoryData.put("preferredCourt", validDefaults.getPreferredCourt());
-        mandatoryData.put("personalInjuryGeneralDamages", "MORE_THAN_THOUSAND_POUNDS");
-        mandatoryData.put("housingDisrepairCostOfRepairDamages", "MORE_THAN_THOUSAND_POUNDS");
-        mandatoryData.put("housingDisrepairOtherDamages", "MORE_THAN_THOUSAND_POUNDS");
-        mandatoryData.put("sotSignerName", validDefaults.getStatementOfTruth().getSignerName());
-        mandatoryData.put("sotSignerRole", validDefaults.getStatementOfTruth().getSignerRole());
+        builder.externalId(validDefaults.getExternalId().toString());
+        builder.referenceNumber("random_reference_number");
+        builder.reason(validDefaults.getReason());
+        builder.amountType(AmountType.BREAK_DOWN);
+        builder.interestType(CcdInterestType.valueOf(interest.getType().name()));
+        builder.interestRate(interest.getRate());
+        builder.interestReason(interest.getReason());
+        builder.interestDateType(CcdInterestDateType.valueOf(interest.getInterestDate().getType().name()));
+        builder.interestClaimStartDate(interest.getInterestDate().getDate());
+        builder.interestStartDateReason(interest.getInterestDate().getReason());
+        builder.interestEndDateType(CcdInterestEndDateType.valueOf(interest.getInterestDate().getEndDateType().name()));
+        builder.paymentId(referencePayment.getId());
+        builder.paymentAmount(referencePayment.getAmount());
+        builder.paymentReference(referencePayment.getReference());
+        builder.paymentStatus(referencePayment.getStatus());
+        builder.paymentDateCreated(LocalDate.parse(referencePayment.getDateCreated()));
+        builder.preferredCourt(validDefaults.getPreferredCourt());
+        builder.personalInjuryGeneralDamages("MORE_THAN_THOUSAND_POUNDS");
+        builder.housingDisrepairCostOfRepairDamages("MORE_THAN_THOUSAND_POUNDS");
+        builder.housingDisrepairOtherDamages("MORE_THAN_THOUSAND_POUNDS");
+        builder.sotSignerName(validDefaults.getStatementOfTruth().getSignerName());
+        builder.sotSignerRole(validDefaults.getStatementOfTruth().getSignerRole());
 
         when(coreCaseDataApi.searchCases(any(), any(), any(), any()))
              .thenReturn(SearchResult.builder().total(1)
-                                               .cases(Arrays.asList(CaseDetails.builder().data(mandatoryData).build()))
+                                               .cases(Arrays.asList(CaseDetails.builder().data(builder.buildMap()).build()))
                                                .build());
 
         // mock idam call
@@ -230,7 +247,9 @@ public class GetClaimIT {
 
         Claim claim = objectMapper.readValue(response.getResponse().getContentAsString(), Claim.class);
 
-        assertThat(validDefaults).isSameAs(claim);
+        assertThat(validDefaults).isEqualToComparingFieldByFieldRecursively(claim);
     }
+
+
 
 }
