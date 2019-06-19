2372053266de3db20b6b5f428efc9723b7b829ba
alexsoliri <alexis.gayte@solirius.com>
2019-06-19 10:12:37 +0100

    remove user creation

--- src/functionalTest/java/uk/gov/hmcts/reform/cmc/submit/BaseFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cmc/submit/BaseFunctionalTest.java
@@ -1,19 +1,14 @@
 package uk.gov.hmcts.reform.cmc.submit;
 
-import feign.FeignException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.IdamTestApi;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
-import uk.gov.hmcts.reform.idam.client.models.test.CreateUserRequest;
-import uk.gov.hmcts.reform.idam.client.models.test.UserGroup;
 
 import javax.annotation.PostConstruct;
 
@@ -50,19 +45,6 @@ public abstract class BaseFunctionalTest {
     }
 
     public User citizen() {
-        try {
-            idamTestApi.createUser(CreateUserRequest.builder()
-                    .email(citizenUsername)
-                    .userGroup(new UserGroup("citizens"))
-                    .password(citizenPassword)
-                    .build());
-        } catch (FeignException e) {
-            if (e.status() == HttpStatus.BAD_REQUEST.value()) {
-                log.info("Bad request from idam, probably user already exists, continuing");
-            } else {
-                throw e;
-            }
-        }
 
         String token = idamClient.authenticateUser(citizenUsername, citizenPassword);
         UserDetails userDetails = idamClient.getUserDetails(token);
